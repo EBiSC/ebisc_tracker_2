1cbca3b21c65024a599346befba6e3bc6dbd6840
streeter <streeter@ebi.ac.uk>
2016-09-19 16:16:28 +0100

    Take connection details on the command line

--- webserver/src/tracker/tracker.go
+++ webserver/src/tracker/tracker.go
@@ -12,18 +12,24 @@ import (
 )
 
 func main() {
-  var dir string
+  var dir, dbhost string
+  var dbInfo mgo.DialInfo
 
-  flag.StringVar(&dir, "dir", ".", "the directory to serve files from. Defaults to the current dir")
+  flag.StringVar(&dir, "dir", "/usr/src/myapp", "the directory to serve files from. Defaults to /usr/src/myapp")
+  flag.StringVar(&dbhost, "dbhost", "mongodb", "mongodb host name. Defaults to mongodb")
+  flag.StringVar(&dbInfo.Username, "dbuser", "ebisc", "mongodb user name. Defaults to ebisc")
+  flag.StringVar(&dbInfo.Password, "dbpass", "ebisc", "mongodb password. Defaults to ebisc")
+  flag.StringVar(&dbInfo.Database, "dbname", "ebisc", "mongodb database name. Defaults to ebisc")
   flag.Parse()
 
-  r := mux.NewRouter()
-  r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir(dir))))
-
-  session, err := mgo.Dial("mongodb://ebisc:ebisc@mongodb/ebisc")
+  dbInfo.Addrs = []string{dbhost}
+  session, err := mgo.DialWithInfo(&dbInfo)
   if err != nil {
     panic(err)
   }
+
+  r := mux.NewRouter()
+  r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir(dir))))
 
   api := r.PathPrefix("/api").Subrouter()
   api.Handle("/test", &apiHandler{testHandlerFn, session});
