8b48642eb496368af27eee3903a1bb16fd2c41f1
streeter <streeter@ebi.ac.uk>
2016-09-20 15:27:49 +0100

    Finalised the module endpoint

--- webserver/src/tracker/api/api_handlers.go
+++ webserver/src/tracker/api/api_handlers.go
@@ -5,6 +5,7 @@ import (
     "gopkg.in/mgo.v2"
     "github.com/go-errors/errors"
     "labix.org/v2/mgo/bson"
+    "sync"
 )
 
 func testHandlerFn(res *apiContent, r *http.Request, db *mgo.Session) *apiError{
@@ -23,8 +24,36 @@ func codeRunHandlerFn(res *apiContent, r *http.Request, session *mgo.Session) *a
     return &apiError{errors.Wrap(err, 1), "Database find error", 500}
   }
 
+  if modules, ok := m["modules"].([]interface{}); ok {
+    var wg sync.WaitGroup
+    wg.Add(len(modules))
+    for i, _ := range modules {
+      go func(i int) {
+        defer wg.Done()
+        expandModule(&modules[i], session)
+      }(i)
+    }
+    wg.Wait()
+  }
+
   delete(m, "_id")
   
   *res = m
   return nil
+}
+
+func expandModule(m *interface{}, session *mgo.Session) {
+  module, ok := (*m).(bson.M)
+  if (!ok) {
+    return
+  }
+  newM := make(bson.M)
+  c := session.DB("ebisc").C("test_module")
+  if err := c.Find(bson.M{"module": module["module"]}).One(&newM); err != nil {
+    return
+  }
+  for key, val := range newM {
+    module[key] = val
+  }
+
 }
