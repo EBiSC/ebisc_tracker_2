92649cd1ea80b96bdc0d50384602bb10fda330c7
streeter <streeter@ebi.ac.uk>
2016-09-29 19:09:40 +0100

    A home page

--- webcontent/app/app.module.ts
+++ webcontent/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
 import { ApiErrorComponent }  from './api-error.component';
 import { PageNotFoundComponent }  from './page-not-found.component';
+import { HomeComponent }  from './home.component';
 
 import { routing, appRoutingProviders} from './app.routing';
 import { QuestionsModule }  from './questions/questions.module';
@@ -11,7 +12,7 @@ import { CoreModule }  from './core/core.module';
 
 @NgModule({
   imports: [ BrowserModule, CoreModule, routing, QuestionsModule ],
-  declarations: [ AppComponent, PageNotFoundComponent, ApiErrorComponent ],
+  declarations: [ AppComponent, PageNotFoundComponent, ApiErrorComponent, HomeComponent ],
   providers: [ appRoutingProviders ],
   bootstrap: [ AppComponent ]
 })
--- webcontent/app/app.routing.ts
+++ webcontent/app/app.routing.ts
@@ -2,8 +2,10 @@ import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { PageNotFoundComponent } from './page-not-found.component';
+import { HomeComponent } from './home.component';
 
 const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
   { path: '**', component: PageNotFoundComponent },
 ];
 
