bfa8d67f3a2ddfbeab5905213a1cfd7cdab2248c
streeter <streeter@ebi.ac.uk>
2016-10-19 12:47:34 +0200

    Button on dates page to show older dates

--- webcontent/app/dates/date-chooser.component.ts
+++ webcontent/app/dates/date-chooser.component.ts
@@ -19,14 +19,41 @@ export class DateChooserComponent implements OnInit {
 
   // public properties
   exams: {date: string}[] = [];
+  hasMore: boolean = false;
+  isLoading: boolean = false;
+
+  // private properties
+  earliestDate: string;
 
   constructor(
     private apiExamService: ApiExamService,
   ){};
 
   ngOnInit() {
-
-    this.apiExamService.getList({})
-      .subscribe((res: ExamList) => this.exams = res.items);
+    this.getList();
   };
+
+  getList() {
+    if (this.isLoading) {
+      return;
+    }
+    let params: {[p:string]: string} = {};
+    if (this.earliestDate) {
+      params['date'] = this.earliestDate;
+    }
+    this.isLoading = true;
+    this.apiExamService.getList(params)
+      .subscribe((res: ExamList) => {
+        let numItems = res.items ? res.items.length : 0;
+        if (numItems > 0) {
+          this.hasMore = true;
+          this.earliestDate = res.items[numItems-1].date;
+          this.exams = this.exams.concat(res.items);
+        }
+        else {
+          this.hasMore = false;
+        }
+        this.isLoading = false;
+      });
+  }
 };
