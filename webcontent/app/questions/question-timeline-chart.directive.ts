877aa1d56fdd7802d31fd193cd69e19f25daba68
streeter <streeter@ebi.ac.uk>
2016-10-30 11:42:06 +0000

    Better cleanup of resources

--- webcontent/app/questions/question-timeline-chart.directive.ts
+++ webcontent/app/questions/question-timeline-chart.directive.ts
@@ -12,7 +12,7 @@ export class QuestionTimelineChartDirective implements OnChanges, OnInit{
   @Input('qTimelineChart') qTimeline: QuestionTimeline;
 
   private gLoadedSubscription: Subscription;
-  private isLoaded: boolean;
+  private chart: google.visualization.Timeline;
 
   constructor(
     private elementRef: ElementRef,
@@ -21,7 +21,7 @@ export class QuestionTimelineChartDirective implements OnChanges, OnInit{
 
   ngOnInit() {
     this.googleLoadedService.load('timeline').subscribe((isLoaded: boolean) => {
-      this.isLoaded = isLoaded;
+      this.chart = new google.visualization.Timeline(this.elementRef.nativeElement);
       this.loadChart();
     });
   }
@@ -34,48 +34,51 @@ export class QuestionTimelineChartDirective implements OnChanges, OnInit{
     if (this.gLoadedSubscription) {
       this.gLoadedSubscription.unsubscribe();
     }
+    if (this.chart) {
+      this.chart.clearChart();
+    }
   };
 
   loadChart() {
-    if (this.qTimeline && this.isLoaded ) {
-      let cols: {type:string, id:string}[] = [
-              {type: 'string', id: 'Term'},
-              {type: 'string', id: 'Num'},
-              {type: 'date', id: 'Start'},
-              {type: 'date', id: 'End'}
-          ];
-      let rows: {c:{v: string|number}[]}[] = [];
-      let lastNum : number;
-      let firstRow = true;
-      this.qTimeline.items.forEach(qEvent => {
-        let qEventDate = Date.parse(qEvent.date);
-        if (!firstRow) {
-          rows[rows.length -1].c[2].v = qEventDate;
-        }
-        if (firstRow || lastNum != qEvent.numFailed) {
-          rows.push({c:[{v: 'How many failed'}, {v: ""+qEvent.numFailed}, {v: qEventDate}, {v: qEventDate}]});
-        }
-        lastNum = qEvent.numFailed;
-        firstRow = false
-      });
-
-      firstRow = true;
-      this.qTimeline.items.forEach(qEvent => {
-        let qEventDate = Date.parse(qEvent.date);
-        if (!firstRow) {
-          rows[rows.length -1].c[2].v = qEventDate;
-        }
-        if (firstRow || lastNum != qEvent.numTested) {
-          rows.push({c:[{v: 'How many tested'}, {v: ""+qEvent.numTested}, {v: qEventDate}, {v: qEventDate}]});
-        }
-        lastNum = qEvent.numTested;
-        firstRow = false
-      });
-
-      let dt = new google.visualization.DataTable({cols:cols, rows:rows}, 0.6);
-      let timeline = new google.visualization.Timeline(this.elementRef.nativeElement);
-      timeline.draw(dt);
+    if (!(this.qTimeline && this.chart )) {
+      return;
     }
+    let cols: {type:string, id:string}[] = [
+            {type: 'string', id: 'Term'},
+            {type: 'string', id: 'Num'},
+            {type: 'date', id: 'Start'},
+            {type: 'date', id: 'End'}
+        ];
+    let rows: {c:{v: string|number}[]}[] = [];
+    let lastNum : number;
+    let firstRow = true;
+    this.qTimeline.items.forEach(qEvent => {
+      let qEventDate = Date.parse(qEvent.date);
+      if (!firstRow) {
+        rows[rows.length -1].c[2].v = qEventDate;
+      }
+      if (firstRow || lastNum != qEvent.numFailed) {
+        rows.push({c:[{v: 'How many failed'}, {v: ""+qEvent.numFailed}, {v: qEventDate}, {v: qEventDate}]});
+      }
+      lastNum = qEvent.numFailed;
+      firstRow = false
+    });
+
+    firstRow = true;
+    this.qTimeline.items.forEach(qEvent => {
+      let qEventDate = Date.parse(qEvent.date);
+      if (!firstRow) {
+        rows[rows.length -1].c[2].v = qEventDate;
+      }
+      if (firstRow || lastNum != qEvent.numTested) {
+        rows.push({c:[{v: 'How many tested'}, {v: ""+qEvent.numTested}, {v: qEventDate}, {v: qEventDate}]});
+      }
+      lastNum = qEvent.numTested;
+      firstRow = false
+    });
+
+    let dt = new google.visualization.DataTable({cols:cols, rows:rows}, 0.6);
+    this.chart.draw(dt);
   }
 
 }
