901b62c2b6073e736c343a9530ecfa04993e9c24
streeter <streeter@ebi.ac.uk>
2016-11-25 09:43:24 +0000

    Time how long each stage takes

--- tracker/run.pl
+++ tracker/run.pl
@@ -12,6 +12,7 @@ use EBiSC::API::IMS;
 use EBiSC::API::Biosamples;
 use EBiSC::API::ECACC;
 use EBiSC::MongoDB;
+use Time::Moment;
 
 my $hpscreg_api = EBiSC::API::hPSCreg->new(
   user => $ENV{HPSCREG_USER} || die( 'HPSCREG_USER not set'),
@@ -28,7 +29,10 @@ my $db = EBiSC::MongoDB->new(
 );
 
 my @service_errors;
+my %timings;
 
+my ($now1, $now2);
+$now1 = Time::Moment->now_utc;
 eval {
   EBiSC::Utils::IMS::sync_db(
     api => $ims_api,
@@ -38,6 +42,9 @@ eval {
 if ($@) {
   push(@service_errors, {service => 'IMS', error => $@});
 }
+$now2 = Time::Moment->now_utc;
+$timings{ims_fetch} = $now1->delta_seconds($now2);
+$now1 = $now2;
 
 eval {
   EBiSC::Utils::hPSCreg::sync_db(
@@ -48,6 +55,9 @@ eval {
 if ($@) {
   push(@service_errors, {service => 'hPSCreg', error => $@});
 }
+$now2 = Time::Moment->now_utc;
+$timings{hpscreg_fetch} = $now1->delta_seconds($now2);
+$now1 = $now2;
 
 eval {
   EBiSC::Utils::Biosamples::sync_db(
@@ -58,6 +68,9 @@ eval {
 if ($@) {
   push(@service_errors, {service => 'Biosamples', error => $@});
 }
+$now2 = Time::Moment->now_utc;
+$timings{biosamples_fetch} = $now1->delta_seconds($now2);
+$now1 = $now2;
 
 eval {
   EBiSC::Utils::ECACC::sync_db(
@@ -68,6 +81,9 @@ eval {
 if ($@) {
   push(@service_errors, {service => 'ECACC', error => $@});
 }
+$now2 = Time::Moment->now_utc;
+$timings{ecacc_fetch} = $now1->delta_seconds($now2);
+$now1 = $now2;
 
 eval {
   $db->exam->ensure_indexes;
@@ -81,8 +97,12 @@ if ($@) {
 my $exam = EBiSC::Utils::Question::run_questions(
   db => $db
 );
+$now2 = Time::Moment->now_utc;
+$timings{ask_questions} = $now1->delta_seconds($now2);
+$now1 = $now2;
 
 $exam->{serviceErrors} = \@service_errors;
+$exam->{timings} = \%timings;
 
 EBiSC::Utils::Question::commit_exam (
   db => $db,
